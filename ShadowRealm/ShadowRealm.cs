ecf3dc1c6524d013542cc42401e5abd3066e6603
Keelhauled <keelhauledhs@gmail.com>
2019-05-30 06:34:11 +0300

    Cleaner shadow fix

--- ShadowRealm/ShadowRealm.cs
+++ ShadowRealm/ShadowRealm.cs
@@ -1,6 +1,9 @@
 ﻿using BepInEx;
 using Harmony;
 using Studio;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
 
 namespace ShadowRealm
 {
@@ -25,27 +28,29 @@ namespace ShadowRealm
         }
         #endif
 
-        [HarmonyPrefix, HarmonyPatch(typeof(Studio.Studio), nameof(Studio.Studio.AddLight), new[] { typeof(int) })]
-        public static bool StudioLightFix(Studio.Studio __instance, ref int _no)
-        {
-            var ocilight = AddObjectLight.Add(_no);
-            ocilight.light.cullingMask |= layerValue;
-
-            UndoRedoManager.Instance.Clear();
-
-            if(Studio.Studio.optionSystem.autoHide)
-                __instance.rootButtonCtrl.OnClick(-1);
-
-            if(Studio.Studio.optionSystem.autoSelect && ocilight != null)
-                __instance.treeNodeCtrl.SelectSingle(ocilight.treeNodeObject, true);
-
-            return false;
-        }
-
         [HarmonyPostfix, HarmonyPatch(typeof(AddObjectLight), nameof(AddObjectLight.Load), new[] { typeof(OILightInfo), typeof(ObjectCtrlInfo), typeof(TreeNodeObject) })]
-        public static void LoadLightFix(OCILight __result)
+        public static void LoadLightCullingMask(OCILight __result)
         {
             __result.light.cullingMask |= layerValue;
+        }
+
+        [HarmonyPostfix, HarmonyPatch(typeof(AddObjectLight), nameof(AddObjectLight.Add), new[] { typeof(int) })]
+        public static void AddLightCullingMask(OCILight __result)
+        {
+            __result.light.cullingMask |= layerValue;
+        }
+
+        [HarmonyTranspiler, HarmonyPatch(typeof(Studio.Studio), nameof(Studio.Studio.AddLight), new[] { typeof(int) })]
+        public static IEnumerable<CodeInstruction> UnlimitedLights(IEnumerable<CodeInstruction> instructions)
+        {
+            var codes = instructions.ToList();
+
+            codes[0].opcode = OpCodes.Nop;
+            codes[1].opcode = OpCodes.Nop;
+            codes[2].opcode = OpCodes.Nop;
+            codes[3].opcode = OpCodes.Br;
+
+            return codes;
         }
     }
 }
