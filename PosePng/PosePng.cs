c1081925923c0a6ed7a241cec99d4b9f0229e007
Keelhauled <keelhauledhs@gmail.com>
2019-06-02 22:52:41 +0300

    Add png poses to list

--- PosePng/PosePng.cs
+++ PosePng/PosePng.cs
@@ -14,8 +14,10 @@ namespace PosePng
     class PosePng : BaseUnityPlugin
     {
         public const string Version = "1.0.0";
+
         const string PngExt = ".png";
         HarmonyInstance harmony;
+        static bool patched = false;
 
         void Start()
         {
@@ -33,20 +35,19 @@ namespace PosePng
         [HarmonyPrefix, HarmonyPatch(typeof(PauseCtrl), nameof(PauseCtrl.Save))]
         public static bool PoseSavePatch(OCIChar _ociChar, ref string _name)
         {
-            var path = UserData.Create(PauseCtrl.savePath) + $"{_name}_{DateTime.Now.ToString("yyyy_MMdd_HHmm_ss_fff")}.png";
+            var path = UserData.Create(PauseCtrl.savePath) + $"{_name}_{DateTime.Now.ToString("yyyy_MMdd_HHmm_ss_fff")}{PngExt}";
             var fileInfo = new PauseCtrl.FileInfo(_ociChar);
+
             using(var fileStream = new FileStream(path, FileMode.Create, FileAccess.Write))
+            using(var binaryWriter = new BinaryWriter(fileStream))
             {
-                using(var binaryWriter = new BinaryWriter(fileStream))
-                {
-                    var buffer = Studio.Studio.Instance.gameScreenShot.CreatePngScreen(320, 180, false, false);
-                    binaryWriter.Write(buffer);
-                    binaryWriter.Write(PauseCtrl.saveIdentifyingCode);
-                    binaryWriter.Write(PauseCtrl.saveVersion);
-                    binaryWriter.Write(_ociChar.oiCharInfo.sex);
-                    binaryWriter.Write(_name);
-                    fileInfo.Save(binaryWriter);
-                }
+                var buffer = Studio.Studio.Instance.gameScreenShot.CreatePngScreen(320, 180, false, false);
+                binaryWriter.Write(buffer);
+                binaryWriter.Write(PauseCtrl.saveIdentifyingCode);
+                binaryWriter.Write(PauseCtrl.saveVersion);
+                binaryWriter.Write(_ociChar.oiCharInfo.sex);
+                binaryWriter.Write(_name);
+                fileInfo.Save(binaryWriter);
             }
 
             return false;
@@ -59,22 +60,20 @@ namespace PosePng
             {
                 var fileInfo = new PauseCtrl.FileInfo(null);
                 using(var fileStream = new FileStream(_path, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+                using(var binaryReader = new BinaryReader(fileStream))
                 {
-                    using(var binaryReader = new BinaryReader(fileStream))
+                    PngFile.SkipPng(binaryReader);
+
+                    if(string.Compare(binaryReader.ReadString(), PauseCtrl.saveIdentifyingCode) != 0)
                     {
-                        PngFile.SkipPng(binaryReader);
-
-                        if(string.Compare(binaryReader.ReadString(), PauseCtrl.saveIdentifyingCode) != 0)
-                        {
-                            __result = false;
-                            return false;
-                        }
-
-                        int ver = binaryReader.ReadInt32();
-                        binaryReader.ReadInt32();
-                        binaryReader.ReadString();
-                        fileInfo.Load(binaryReader, ver);
+                        __result = false;
+                        return false;
                     }
+
+                    int ver = binaryReader.ReadInt32();
+                    binaryReader.ReadInt32();
+                    binaryReader.ReadString();
+                    fileInfo.Load(binaryReader, ver);
                 }
 
                 fileInfo.Apply(_ociChar);
@@ -91,21 +90,19 @@ namespace PosePng
             if(Path.GetExtension(_path).ToLower() == PngExt)
             {
                 using(var fileStream = new FileStream(_path, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+                using(var binaryReader = new BinaryReader(fileStream))
                 {
-                    using(var binaryReader = new BinaryReader(fileStream))
-                    {
-                        PngFile.SkipPng(binaryReader);
+                    PngFile.SkipPng(binaryReader);
 
-                        if(string.Compare(binaryReader.ReadString(), PauseCtrl.saveIdentifyingCode) != 0)
-                        {
-                            __result = "";
-                        }
-                        else
-                        {
-                            binaryReader.ReadInt32();
-                            binaryReader.ReadInt32();
-                            __result = binaryReader.ReadString();
-                        }
+                    if(string.Compare(binaryReader.ReadString(), PauseCtrl.saveIdentifyingCode) != 0)
+                    {
+                        __result = "";
+                    }
+                    else
+                    {
+                        binaryReader.ReadInt32();
+                        binaryReader.ReadInt32();
+                        __result = binaryReader.ReadString();
                     }
                 }
                 
@@ -120,18 +117,19 @@ namespace PosePng
         {
             var codes = instructions.ToList();
             var magic = "System.Collections.Generic.List`1[System.String] listPath";
-            
-            for(int i = 0; i < codes.Count; i++)
+
+            if(!patched)
             {
-                var code = codes[i];
-                if(code.opcode == OpCodes.Ldfld && code.operand.ToString() == magic)
+                for(int i = 0; i < codes.Count; i++)
                 {
-                    var method = AccessTools.Method(typeof(PosePng), nameof(AddPngToPauseRegistrationList));
-                    Console.WriteLine(method);
-                    codes.Insert(i + 1, new CodeInstruction(OpCodes.Call, method));
-                    Console.WriteLine("Patched InitList");
-                    break;
-                }
+                    if(codes[i].opcode == OpCodes.Ldfld && codes[i].operand.ToString() == magic)
+                    {
+                        var method = AccessTools.Method(typeof(PosePng), nameof(AddPngToPauseRegistrationList));
+                        codes.Insert(i + 1, new CodeInstruction(OpCodes.Call, method));
+                        patched = true;
+                        break;
+                    }
+                } 
             }
 
             return codes;
@@ -144,31 +142,28 @@ namespace PosePng
             var sex = traverse.Field("m_OCIChar").GetValue<OCIChar>().oiCharInfo.sex;
             var pngFiles = Directory.GetFiles(UserData.Create(PauseCtrl.savePath), $"*{PngExt}").Where(x => CheckIdentifyingCode(x, sex));
             var listPath = traverse.Field("listPath").GetValue<List<string>>();
-            foreach(var item in pngFiles)
-            {
-                Console.WriteLine(item);
-                listPath.Add(item);
-            }
 
-            Console.WriteLine("Png added to PauseRegistrationList");
+            foreach(var path in pngFiles)
+            {
+                if(!listPath.Contains(path))
+                    listPath.Add(path);
+            }
         }
 
         public static bool CheckIdentifyingCode(string _path, int _sex)
         {
             using(var fileStream = new FileStream(_path, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+            using(var binaryReader = new BinaryReader(fileStream))
             {
-                using(var binaryReader = new BinaryReader(fileStream))
-                {
-                    PngFile.SkipPng(binaryReader);
+                PngFile.SkipPng(binaryReader);
 
-                    if(string.Compare(binaryReader.ReadString(), PauseCtrl.saveIdentifyingCode) != 0)
-                        return false;
+                if(string.Compare(binaryReader.ReadString(), PauseCtrl.saveIdentifyingCode) != 0)
+                    return false;
 
-                    binaryReader.ReadInt32();
+                binaryReader.ReadInt32();
 
-                    if(_sex != binaryReader.ReadInt32())
-                        return false;
-                }
+                if(_sex != binaryReader.ReadInt32())
+                    return false;
             }
 
             return true;
