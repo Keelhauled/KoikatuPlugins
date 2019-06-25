2c1336b1d863d00b19cfd3652b0a89d6edf080ff
Keelhauled <keelhauledhs@gmail.com>
2019-06-25 03:36:40 +0300

    Check if posepng path is good

--- PosePng/PosePng.cs
+++ PosePng/PosePng.cs
@@ -2,10 +2,8 @@
 using Harmony;
 using Studio;
 using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Reflection.Emit;
+using BepInEx.Logging;
 
 namespace PosePng
 {
@@ -15,12 +13,15 @@ namespace PosePng
     {
         public const string Version = "1.0.0";
 
+        public static ConfigWrapper<string> SavePath { get; set; }
+
         const string PngExt = ".png";
         HarmonyInstance harmony;
-        static bool patched = false;
 
         void Start()
         {
+            SavePath = new ConfigWrapper<string>("SavePath", this, "");
+
             harmony = HarmonyInstance.Create("keelhauled.posepng.harmony");
             harmony.PatchAll(GetType());
         }
@@ -35,19 +36,28 @@ namespace PosePng
         [HarmonyPrefix, HarmonyPatch(typeof(PauseCtrl), nameof(PauseCtrl.Save))]
         public static bool PoseSavePatch(OCIChar _ociChar, ref string _name)
         {
-            var path = UserData.Create(PauseCtrl.savePath) + $"{_name}_{DateTime.Now.ToString("yyyy_MMdd_HHmm_ss_fff")}{PngExt}";
+            var filename = $"{_name}_{DateTime.Now.ToString("yyyy_MMdd_HHmm_ss_fff")}{PngExt}";
+            var path = Path.Combine(SavePath.Value, filename);
             var fileInfo = new PauseCtrl.FileInfo(_ociChar);
 
-            using(var fileStream = new FileStream(path, FileMode.Create, FileAccess.Write))
-            using(var binaryWriter = new BinaryWriter(fileStream))
+            try
             {
-                var buffer = Studio.Studio.Instance.gameScreenShot.CreatePngScreen(320, 180, false, false);
-                binaryWriter.Write(buffer);
-                binaryWriter.Write(PauseCtrl.saveIdentifyingCode);
-                binaryWriter.Write(PauseCtrl.saveVersion);
-                binaryWriter.Write(_ociChar.oiCharInfo.sex);
-                binaryWriter.Write(_name);
-                fileInfo.Save(binaryWriter);
+                using(var fileStream = new FileStream(path, FileMode.Create, FileAccess.Write))
+                using(var binaryWriter = new BinaryWriter(fileStream))
+                {
+                    var buffer = Studio.Studio.Instance.gameScreenShot.CreatePngScreen(320, 180, false, false);
+                    binaryWriter.Write(buffer);
+                    binaryWriter.Write(PauseCtrl.saveIdentifyingCode);
+                    binaryWriter.Write(PauseCtrl.saveVersion);
+                    binaryWriter.Write(_ociChar.oiCharInfo.sex);
+                    binaryWriter.Write(_name);
+                    fileInfo.Save(binaryWriter);
+                }
+            }
+            catch(Exception ex)
+            {
+                Logger.Log(LogLevel.Message, "PosePng plugin save path has not been set properly");
+                Logger.Log(LogLevel.Error, ex);
             }
 
             return false;
