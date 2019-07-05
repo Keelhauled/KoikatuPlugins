2dd6bc300c8a96325e496bc80cb96e2936cdb5bb
Keelhauled <keelhauledhs@gmail.com>
2019-07-05 06:20:14 +0300

    Bounds stuff

---
+++ StudioUX/BoundsUtils.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace StudioUX
+{
+    public static class BoundsUtils
+    {
+        public static Bounds CombineBounds(Stack<SkinnedMeshRenderer> skins)
+        {
+            var bounds = skins.Pop().bounds;
+
+            while(skins.Count > 0)
+            {
+                var skin = skins.Pop();
+                bounds.Encapsulate(skin.bounds);
+            }
+
+            return bounds;
+        }
+
+        public static Rect BoundsToScreenRect(Bounds bounds, Camera camera)
+        {
+            Vector3 cen = bounds.center;
+            Vector3 ext = bounds.extents;
+            Vector2[] extentPoints = new Vector2[8]
+            {
+                camera.WorldToScreenPoint(new Vector3(cen.x-ext.x, cen.y-ext.y, cen.z-ext.z)),
+                camera.WorldToScreenPoint(new Vector3(cen.x+ext.x, cen.y-ext.y, cen.z-ext.z)),
+                camera.WorldToScreenPoint(new Vector3(cen.x-ext.x, cen.y-ext.y, cen.z+ext.z)),
+                camera.WorldToScreenPoint(new Vector3(cen.x+ext.x, cen.y-ext.y, cen.z+ext.z)),
+                camera.WorldToScreenPoint(new Vector3(cen.x-ext.x, cen.y+ext.y, cen.z-ext.z)),
+                camera.WorldToScreenPoint(new Vector3(cen.x+ext.x, cen.y+ext.y, cen.z-ext.z)),
+                camera.WorldToScreenPoint(new Vector3(cen.x-ext.x, cen.y+ext.y, cen.z+ext.z)),
+                camera.WorldToScreenPoint(new Vector3(cen.x+ext.x, cen.y+ext.y, cen.z+ext.z))
+            };
+
+            Vector2 min = extentPoints[0];
+            Vector2 max = extentPoints[0];
+            foreach(Vector2 v in extentPoints)
+            {
+                min = Vector2.Min(min, v);
+                max = Vector2.Max(max, v);
+            }
+
+            return new Rect(min.x, min.y, max.x - min.x, max.y - min.y);
+        }
+
+        public static Vector2 WorldToGUIPoint(Vector3 world)
+        {
+            Vector2 screenPoint = Camera.main.WorldToScreenPoint(world);
+            screenPoint.y = (float)Screen.height - screenPoint.y;
+            return screenPoint;
+        }
+    }
+}
--- StudioUX/StudioUX.cs
+++ StudioUX/StudioUX.cs
@@ -1,43 +1,90 @@
-﻿using System;
+﻿using BepInEx;
+using BepInEx.Harmony;
+using BepInEx.Logging;
+using HarmonyLib;
+using Studio;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using BepInEx;
+using System.Reflection.Emit;
 using UnityEngine;
 using Vectrosity;
-using Studio;
-using BepInEx.Logging;
 
 namespace StudioUX
 {
     [BepInPlugin("keelhauled.studiouxexperiment", "StudioUX", "1.0.0")]
     public class StudioUX : BaseUnityPlugin
     {
-        List<VectorLine> lines = new List<VectorLine>();
+        new static ManualLogSource Logger;
 
         void Start()
         {
-            var characters = FindObjectsOfType<ChaControl>();
+            Logger = base.Logger;
+            StartCoroutine(EndOfFrame());
 
+            var characters = GuideObjectManager.Instance.selectObjectKey.Select(x => Studio.Studio.GetCtrlInfo(x) as OCIChar).Where(x => x != null);
             foreach(var chara in characters)
             {
-                foreach(var renderer in chara.GetComponentsInChildren<Renderer>())
+                var skins = chara.charInfo.GetComponentsInChildren<SkinnedMeshRenderer>();
+                foreach(var skin in skins) skin.updateWhenOffscreen = true;
+                var updateSkins = new List<SkinnedMeshRenderer> { chara.charInfo.rendBody as SkinnedMeshRenderer, chara.charInfo.rendFace as SkinnedMeshRenderer };
+
+                //var bounds3d = new VectorLineUpdater();
+                //bounds3d.VectorLine = new VectorLine("Bounds3D", new List<Vector3>(24), 1f, LineType.Discrete);
+                //bounds3d.Update = () =>
+                //{
+                //    var bounds = BoundsUtils.CombineBounds(new Stack<SkinnedMeshRenderer>(updateSkins));
+                //    bounds3d.VectorLine.MakeCube(bounds.center, bounds.size.x, bounds.size.y, bounds.size.z);
+                //    bounds3d.VectorLine.SetColor(Color.red);
+                //    bounds3d.VectorLine.Draw();
+                //};
+
+                var bounds2d = new VectorLineUpdater();
+                bounds2d.VectorLine = new VectorLine("Bounds2D", new List<Vector2>(8), 1f, LineType.Discrete);
+                bounds2d.Update = () =>
                 {
-                    var points = new List<Vector3>(24);
-                    var line = new VectorLine("Cube", points, 2f);
-                    line.MakeCube(renderer.bounds.center, renderer.bounds.extents.x, renderer.bounds.extents.z, renderer.bounds.extents.y);
-                    lines.Add(line); 
-                }
+                    var bounds = BoundsUtils.CombineBounds(new Stack<SkinnedMeshRenderer>(updateSkins));
+                    var rect = BoundsUtils.BoundsToScreenRect(bounds, Camera.main);
+                    bounds2d.VectorLine.MakeRect(rect);
+                    bounds2d.VectorLine.SetColor(Color.green);
+                    bounds2d.VectorLine.Draw();
+                };
+            }
+        }
+
+        IEnumerator EndOfFrame()
+        {
+            while(true)
+            {
+                yield return new WaitForEndOfFrame();
+                VectorLineUpdater.UpdateAllLines();
             }
         }
 
         void Update()
         {
-            lines.ForEach(x => x.Draw());
+            if(Input.GetMouseButtonDown(1))
+            {
+                var mousePos = Input.mousePosition;
+                Logger.Log(LogLevel.Info, mousePos);
+            }
         }
 
         void OnDestroy()
         {
-            VectorLine.Destroy(lines);
+            VectorLineUpdater.DeleteAllLines();
         }
+    }
+
+    public class VectorLineUpdater
+    {
+        public VectorLine VectorLine;
+        public Action Update;
+
+        public static List<VectorLineUpdater> lines = new List<VectorLineUpdater>();
+        public VectorLineUpdater() => lines.Add(this);
+        public static void UpdateAllLines() => lines.ForEach(x => x.Update());
+        public static void DeleteAllLines() => lines.ForEach(x => VectorLine.Destroy(ref x.VectorLine));
     }
 }
