bbf86ad4c0b5edb5b0be850aeab95ced173b7011
Keelhauled <keelhauledhs@gmail.com>
2019-07-30 22:24:21 +0300

    Use shared build output path

--- BuildRelease.bat
+++ BuildRelease.bat
@@ -1,3 +1,3 @@
 del "KoikatuPlugins.zip"
-echo F|xcopy "README.md" "PackingFolder\README.md" /Y
-for /d %%X in (PackingFolder) do "%PROGRAMFILES%\7-Zip\7z.exe" a -tzip -mx7 "KoikatuPlugins.zip" ".\%%X\*"
+echo F|xcopy "README.md" "bin\README.md" /Y
+for /d %%X in (bin) do "%PROGRAMFILES%\7-Zip\7z.exe" a -tzip -mx7 "KoikatuPlugins.zip" ".\%%X\*"
