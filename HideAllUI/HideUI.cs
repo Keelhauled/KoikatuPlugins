c55756e10de9ca193efc275fface86fc67b9bcc1
Keelhauled <keelhauledhs@gmail.com>
2019-07-30 17:42:59 +0300

    HideAllUI plugin

---
+++ HideAllUI/HideUI.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BepInEx;
+using Harmony;
+using ConfigurationManager;
+using UnityEngine;
+
+namespace HideAllUI
+{
+    [BepInPlugin(GUID, "Hide All UI", Version)]
+    public class HideAllUI : BaseUnityPlugin
+    {
+        public const string GUID = "keelhauled.hideallui";
+        public const string Version = "1.0.0";
+
+        SavedKeyboardShortcut HideHotkey;
+
+        void Awake()
+        {
+            HideHotkey = new SavedKeyboardShortcut("HideHotkey", this, new KeyboardShortcut(KeyCode.Space));
+        }
+
+        void Update()
+        {
+            if(HideHotkey.IsDown())
+                ToggleUI();
+        }
+
+        void ToggleUI()
+        {
+
+        }
+    }
+}
